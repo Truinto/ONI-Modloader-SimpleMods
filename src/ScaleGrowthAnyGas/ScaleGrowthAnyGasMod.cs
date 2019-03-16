b2ad26ab4588566d8c22671b86be13b77056a58d
Truinto <47917181+Truinto@users.noreply.github.com>
2019-03-16 11:21:28 +0100

    Update

---
+++ src/ScaleGrowthAnyGas/ScaleGrowthAnyGasMod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace FastTravelTube
+{
+	[HarmonyPatch(typeof(Game), "OnPrefabInit")]
+	internal class ScaleGrowthAnyGasMod_Game_OnPrefabInit
+	{
+		private static void Postfix(Game __instance)
+		{
+			Debug.Log(" === ScaleGrowthAnyGasMod_Game_OnPrefabInit Postfix === ");
+		}
+    }
+
+    [HarmonyPatch(typeof(ScaleGrowthMonitor), "IsInCorrectAtmosphere")]
+    internal class ScaleGrowthAnyGas_ScaleGrowthMonitor_IsInCorrectAtmosphere
+    {
+        private static void Postfix(ref bool __result, ScaleGrowthMonitor.Instance smi)
+        {
+            __result = true;
+        }
+    }
+}
