7a383afbdd2652b4c1a9c058b483fd2ee27c3058
Truinto <47917181+Truinto@users.noreply.github.com>
2019-08-13 22:17:18 +0200

    first release (V2)
    needed the steam id for first release, so this is already V2

---
+++ src/ScaleGrowthAnyGas/CarePackagesMod/CarePackageMod.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using ONI_Common;
+using System.Collections.Generic;
+
+namespace CarePackageMod
+{
+    [HarmonyPatch(typeof(Immigration), "ConfigureCarePackages")]
+    internal class Immigration_ConfigureCarePackages
+    {
+        private static bool Prefix(ref CarePackageInfo[] ___carePackages)
+        {
+            if (! CarePackageState.StateManager.State.enabled) return true;
+
+            List<CarePackageInfo> carePackages = new List<CarePackageInfo>();
+
+            Debug.Log("Setting up care packages:");
+            foreach (KeyValuePair<string, float> info in CarePackageState.StateManager.State.CarePackages)
+            {
+                Debug.Log(" id: " + info.Key + " quantity: " + info.Value);
+                carePackages.Add(new CarePackageInfo(info.Key, info.Value, null));
+            }
+
+            ___carePackages = carePackages.ToArray();
+
+            return false;
+        }
+    }
+}
