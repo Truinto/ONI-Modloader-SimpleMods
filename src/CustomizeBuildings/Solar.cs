7ab7da2dec24b43db6d71e018bfaa38d49837fe5
Truinto <47917181+Truinto@users.noreply.github.com>
2022-01-28 08:11:44 +0100

    cleanup

--- src/CustomizeBuildings/Solar.cs
+++ src/CustomizeBuildings/Solar.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using Common;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
@@ -6,62 +7,68 @@ using System.Reflection.Emit;
 namespace CustomizeBuildings
 {
     [HarmonyPatch(typeof(SolarPanel), "EnergySim200ms")]
-    internal class SolarPanel_SolarMaxPower
+    public class SolarPanel_SolarMaxPower
     {
-        internal static bool Prepare()
+        public static bool Prepare()
         {
             return CustomizeBuildingsState.StateManager.State.SolarMaxPower != 380f;
         }
 
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
         {
-            List<CodeInstruction> code = instr.ToList();
-
-            foreach (CodeInstruction codeInstruction in code)
+            bool flag1 = true;
+            foreach (var codeInstruction in instr)
             {
                 if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 380f)
                 {
                     codeInstruction.operand = CustomizeBuildingsState.StateManager.State.SolarMaxPower;
+                    flag1 = false;
                 }
                 yield return codeInstruction;
             }
+
+            if (flag1)
+                Helpers.Print("Error patch SolarPanel_SolarMaxPower failed");
         }
     }
 
     [HarmonyPatch(typeof(SolarPanelConfig), "CreateBuildingDef")]
-    internal class SolarPanelConfig_CreateBuildingDef
+    public class SolarPanelConfig_CreateBuildingDef
     {
-        internal static bool Prepare()
+        public static bool Prepare()
         {
             return CustomizeBuildingsState.StateManager.State.SolarMaxPower != 380f;
         }
 
-        internal static void Postfix(BuildingDef __result)
+        public static void Postfix(BuildingDef __result)
         {
             __result.GeneratorWattageRating = CustomizeBuildingsState.StateManager.State.SolarMaxPower;
         }
     }
 
     [HarmonyPatch(typeof(SolarPanel), "EnergySim200ms")]
-    internal class SolarPanel_SolarEnergyMultiplier
+    public class SolarPanel_SolarEnergyMultiplier
     {
-        internal static bool Prepare()
+        public static bool Prepare()
         {
             return CustomizeBuildingsState.StateManager.State.SolarEnergyMultiplier != 1f;
         }
 
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
         {
-            List<CodeInstruction> code = instr.ToList();
-
-            foreach (CodeInstruction codeInstruction in code)
+            bool flag1 = true;
+            foreach (var codeInstruction in instr)
             {
                 if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.00053f)
                 {
                     codeInstruction.operand = 0.00053f * CustomizeBuildingsState.StateManager.State.SolarEnergyMultiplier;
+                    flag1 = false;
                 }
                 yield return codeInstruction;
             }
+
+            if (flag1)
+                Helpers.Print("Error patch SolarPanel_SolarEnergyMultiplier failed");
         }
     }
 }
