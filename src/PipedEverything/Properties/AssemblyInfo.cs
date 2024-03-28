b9324185039100810e85b35c2cc0a8a61505e16e
Truinto <47917181+Truinto@users.noreply.github.com>
2024-03-28 18:04:56 +0100

    [1.0.11.0] U51-600112-S - added option to disable atmosphere checks (for oil refinery, oxygen generators)

--- src/PipedEverything/Properties/AssemblyInfo.cs
+++ src/PipedEverything/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("PipedEverything")]
 [assembly: AssemblyProduct("PipedEverything")]
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.11.1")]
+[assembly: AssemblyFileVersion("1.0.11.1")]
