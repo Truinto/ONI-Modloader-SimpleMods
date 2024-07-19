a1ac40a29c1232426e4b34361bf83a0342d3d71d
Truinto <47917181+Truinto@users.noreply.github.com>
2024-07-20 00:21:41 +0200

    [1.0.2.0] U52-622509-SC (release) - fix issue with GameTags

--- src/SharedStorage/Changelog.md
+++ src/SharedStorage/Changelog.md
@@ -1,4 +1,7 @@
 # Changelog
 
-## [1.0.1.0]  U48-568201-S
+## [1.0.2.0] U52-622509-SC (release)
+- fix issue with GameTags
+
+## [1.0.1.0] U48-568201-S
 - first release
--- src/SharedStorage/Properties/AssemblyInfo.cs
+++ src/SharedStorage/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("SharedStorage")]
 [assembly: AssemblyProduct("SharedStorage")]
-[assembly: AssemblyVersion("1.0.1.29")]
-[assembly: AssemblyFileVersion("1.0.1.29")]
+[assembly: AssemblyVersion("1.0.2.10")]
+[assembly: AssemblyFileVersion("1.0.2.10")]
