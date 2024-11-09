957086ee5fc5a2afa4e34006e974ddacfd3b7c75
Truinto <47917181+Truinto@users.noreply.github.com>
2024-11-09 11:50:26 +0100

    ## [1.0.98.0] U52-626616-SC (release) - fixed calculation of skill points

--- src/CustomizeBuildings/Changelog.md
+++ src/CustomizeBuildings/Changelog.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## [1.0.98.0] U52-626616-SC (release)
+- fixed calculation of skill points
+
 ## [1.0.97.0] U52-626616-SC (release)
 - again reapplying skill station after completion, but added check to have enough skill points
 
--- src/CustomizeBuildings/Properties/AssemblyInfo.cs
+++ src/CustomizeBuildings/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("CustomizeBuildings")]
 [assembly: AssemblyProduct("CustomizeBuildings")]
-[assembly: AssemblyVersion("1.0.97.1")]
-[assembly: AssemblyFileVersion("1.0.97.1")]
+[assembly: AssemblyVersion("1.0.98.0")]
+[assembly: AssemblyFileVersion("1.0.98.0")]
